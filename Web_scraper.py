985764035d093d57b4884458da7200a9655ed464
MatthewK <matt.klemick@xtra.co.nz>
2021-03-02 15:51:38 +1300

    Breakpoint push

--- Web_scraper.py
+++ Web_scraper.py
@@ -7,33 +7,37 @@ leaddetals = []
 leadname = []
 leadnumber = []
 leadaddress = []
+leadurl = []
+
 # this code scrapes the web page
 def scape(cat):
+    leadurl = []
     pagenumber = 0
-      
+    leadurlN = 0  
     pagehtml = requests.get("https://www.nzdirectory.co.nz/" + cat + ".html")
     html = BeautifulSoup(pagehtml.text, 'html.parser')
     pages = len(html.find_all('div', class_="pages"))
-      
+    
+
     for page in range(pages):
                   
         if page == 1:
             pagehtml = requests.get("https://www.nzdirectory.co.nz/" + cat + ".html")
-            html = BeautifulSoup(pagehtml.text, 'html.parser')
-            lead_items = html.find_all('div', class_="listing_content")
-            detals = ""
-            
         else:
             pagehtml = requests.get("https://www.nzdirectory.co.nz/" + cat + "-"+ str(page) + ".html")
-            html = BeautifulSoup(pagehtml.text, 'html.parser')
-            lead_items = html.find_all('div', class_="listing_content")
-            detals = ""
-      
+
+        html = BeautifulSoup(pagehtml.text, 'html.parser')
+        lead_items = html.find_all('div', class_="listing_content")
+        url = ""
+        detals = ""
+        
+
         for lead in lead_items:
 
             if lead.find('p',class_="address") in lead:
 
                 detals=(lead.find('p',class_="address")).text
+                url = html.find('li',class_="review")
 
                 if detals.find("+") != -1:
                       
@@ -43,7 +47,16 @@ def scape(cat):
                     leadaddress.append(detals[0:detals.find("+")-1])
                     detals = detals[:0] +  detals[detals.find("+"):]
                     leadnumber.append(detals)
+                    leadurl.append("error")
+                    # url = url[:0] + url[url.find("?")+1:]
+                    # leadurl.append(url[0:url.find("#")])
+                    # url = url[url.find("</li>"):]
+            else:
+                T = False
+                # url = url[url.find("</li>"):]
 
-    return leadname,leadaddress,leadnumber
+    return leadname,leadaddress,leadnumber,leadurl
 #https://www.nzdirectory.co.nz/health-fitness.html
+#<li class="review"><a href="profile/listed.php?www.activeagain.co.nz#reviews" title="Write a review about this listing" rel="nofollow">Review</a></li>
 
+print(scape("health-fitness"))
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 import Web_scraper
 main = Flask(__name__)
 
-#cd \W3T1Project_Car_Scout\E\Scripts
+#cd C:\Users\mattk\Documents\Techtorium\year 2\python\weak3\W3T1Project_Car_Scout\E\Scripts
 #activate
 #set FLASK_APP=main.py
 #flask run 
@@ -12,6 +12,6 @@ print(Web_scraper.scape("health-fitness"))
 
 @main.route('/')
 def index():
-    LN , LA , Ln = Web_scraper.scape("health-fitness")
+    LN , LA , Ln , LU = Web_scraper.scape("health-fitness")
     length = len(LN)
-    return render_template('Scrapeddata.html',name = LN, address = LA, number = Ln, Length = length )
+    return render_template('Scrapeddata.html', name = LN , address = LA, number = Ln , URL = LU , Length = length )
